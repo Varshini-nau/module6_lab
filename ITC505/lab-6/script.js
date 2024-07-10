1921cf749705606e4d4ee0a1756b44fde5011c64
Varshini-nau <sg3575@nau.edu>
2024-07-10 13:38:37 -0700

    Add files via upload

---
+++ ITC505/lab-6/script.js
@@ -0,0 +1,4 @@
+document.getElementById('action-button').addEventListener('click', function() {
+    var contentArea = document.getElementById('interactive-content');
+    contentArea.innerHTML = "<p>Button Clicked!</p>";
+});
